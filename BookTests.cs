0f578a4b0d5315f86cc3fc63f8bda31da3ea178c
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2020-05-02 16:32:55 -0400

    Testing api (#1)
    * Add AddBookPrice test
    
    * Add TestMemberDiscount test
    
    Co-authored-by: Christopher Zeng <zeng.519@osu.edu>

---
+++ BookTests.cs
@@ -0,0 +1,45 @@
+using Xunit;
+using Fisher.Bookstore.Models;
+
+namespace Fisher.Bookstore.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void AddBookPrice()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Acr
+            book.Price = 7.99;
+
+            //Assert
+            Assert.Equal(7.99, book.Price);
+        }
+
+        [Fact]
+        public void TestMemberDiscount()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Act
+            book.ApplyMemberDiscount();
+
+            //Assert
+            var discountPrice = 9.99 * .9;
+            Assert.Equal(discountPrice, book.Price);
+        }
+    }
+}
